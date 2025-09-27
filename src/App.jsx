038a17a6feb919e6a94879f150adf21afe66b36c
Gojo-Satoru-git <praveenchandru1209@gmail.com>
2025-09-27 21:18:58 +0530

    speed adjusted

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from "react";
-import NetworkBackground from "./NetworkBackground";
+import React, { useState, useEffect } from 'react';
+import NetworkBackground from './Components/NetworkBackground';
 
 export default function App() {
-  const [color, setColor] = useState("#00ffff");
-  const isDark = true;
+  const [color, setColor] = useState('#00ffff');
+  const isDark = false;
   useEffect(() => {
-    const colors = isDark? ["#220336", "#0a5552", "#0f0f04"] : ["#00ffff", "#b5d533", "#ffff00"];
+    const colors = isDark
+      ? ['#220336', '#0a5552', '#0f0f04']
+      : ['#00ffff', '#b5d533', '#ffff00'];
     let i = 0;
     const interval = setInterval(() => {
       setColor(colors[i % colors.length]);
@@ -17,13 +19,17 @@ export default function App() {
   return (
     <>
       <NetworkBackground
-        speed={0.8}
+        speed={0.5}
         density={50}
         color={color}
         glow={0.6}
         particleSize={2}
       />
-      <h1 style={{ position: "relative", color: "#060202", textAlign: "center" }}>Welcome</h1>
+      <h1
+        style={{ position: 'relative', color: '#ffffff', textAlign: 'center' }}
+      >
+        Welcome
+      </h1>
     </>
   );
 }
--- src/Components/NetworkBackground.jsx
+++ src/Components/NetworkBackground.jsx
@@ -171,7 +171,7 @@ export default function NetworkBackground({
         width: "100%",
         height: "100%",
         zIndex: 0,
-        background: "#ffffff",
+        background: "#000000",
       }}
     />
   );
