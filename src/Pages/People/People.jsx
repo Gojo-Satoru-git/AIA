fdebb6bcd6d2f73d5a4c4c464ed9380c055b7fa5
Rahul-Sundarraj <fftgosr@gmail.com>
2025-09-30 12:25:44 +0530

    removed header and footer in page

--- src/Pages/People/People.jsx
+++ src/Pages/People/People.jsx
@@ -2,16 +2,13 @@ import React from 'react'
 
 import StudentList from './StudentList'
 import AlumniList from './AlumniList'
-import Header from '../../Components/Header'
-import Footer from '../../Components/Footer'
+
 
 function People() {
   return (
     <>
-    <Header />
     <StudentList />
     <AlumniList />
-    <Footer />
     </>
     
   )
