3231b47a711b335539f2d6444c25d818feb34a8a
Gojo-Satoru-git <praveenchandru1209@gmail.com>
2025-09-30 10:36:10 +0530

    Added placements

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from './Components/Footer';
 import Home from './Pages/Home/Home';
 import People from './Pages/People/People';
 import Projects from './Pages/Projects/Projects';
+import Placements from './Pages/Placements/Placements';
 
 export default function App() {
   return (
@@ -12,11 +13,11 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects/>} />
+        <Route path="/projects" element={<Projects />} />
         <Route path="/symposium" element={<div>Symposium Page</div>} />
         <Route path="/gallery" element={<div>Gallery Page</div>} />
         <Route path="/people" element={<People />} />
-        <Route path="/placements" element={<div>Placements Page</div>} />
+        <Route path="/placements" element={<Placements />} />
         <Route path="/events" element={<div>Events Page</div>} />
         <Route path="/achievements" element={<div>Achievements Page</div>} />
       </Routes>
