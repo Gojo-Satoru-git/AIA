4d018d4560fb9b5259a7ec974bbd76a3526217c4
naveen <naveenkumar03202006@gmail.com>
2025-10-13 23:00:42 +0530

    placement page completed

--- src/Components/placementCard/placementcard.jsx
+++ src/Components/placementCard/placementcard.jsx
@@ -1,11 +1,21 @@
-import { Avatar, Box, Card, CardContent, Stack, Typography } from "@mui/material";
-
+import { Avatar, Box, Button, Card, CardActionArea, CardContent, Chip, Dialog, DialogActions, DialogContent, DialogTitle, Divider, IconButton, Stack, Typography } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import WorkIcon from "@mui/icons-material/Work";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { Grid } from "@mui/material";
+import { useState } from "react";
+
 export function Placementcard({logo,company,role,name,location,year,salary}){
-    
+    const [open,setopen]=useState(false);
     return (
         <>
+        
         <Card
       sx={{
 
@@ -19,6 +29,7 @@ export function Placementcard({logo,company,role,name,location,year,salary}){
         
       }}
     >
+      <CardActionArea onClick={()=>{setopen(true)}}>
       <CardContent>
         {/* Header Section */}
         <Box display="flex" alignItems="center" mb={1.5} >
@@ -86,7 +97,178 @@ export function Placementcard({logo,company,role,name,location,year,salary}){
         </Typography>
         </Box>
       </CardContent>
+      </CardActionArea>
     </Card>
+     <Dialog
+          open={open}
+          onClose={()=>{setopen(false)}}
+          maxWidth="md" 
+          fullWidth
+          PaperProps={{
+            sx: { borderRadius: 3, p: 2 },
+          }}
+        >
+          {/* Header */}
+          <DialogTitle sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg"
+              alt="Microsoft"
+              style={{ width: 35 }}
+            />
+            <Box flexGrow={1}>
+              <Typography variant="h6" fontWeight="bold">
+                Microsoft
+              </Typography>
+              <Typography variant="subtitle1"  color="text.secondary">
+                AI Research Engineer
+              </Typography>
+            </Box>
+            <IconButton onClick={()=>{setopen(false)}} size="small">
+              <CloseIcon />
+            </IconButton>
+          </DialogTitle>
+    
+          <Divider />
+    
+          <DialogContent>
+            {/* Student Details */}
+            <Box sx={{ backgroundColor: "#f5f9fb", p: 2, borderRadius: 2, mb: 2 }}>
+              <Stack direction="row" spacing={1}>
+              <WorkspacePremiumIcon />
+              <Typography fontWeight="bold" mb={1}>
+                Student Details
+              </Typography>
+              </Stack>
+              <Grid container rowSpacing={10} columnSpacing={20} sx={{padding:"1rem"}}>
+            <Grid item xs={6} >
+               <Stack  sx={{padding:"0.5rem"}}>
+                <Typography variant="body1" sx={{color:"grey"}}>
+                  Name
+                </Typography>
+                <Typography sx={{fontWeight:"bold"}}>
+                  Sarah Johnson
+                </Typography>
+              </Stack >
+               <Stack sx={{padding:"0.5rem"}}>
+                <Typography variant="body1" sx={{color:"grey"}}>
+                  CGPA
+                </Typography>
+                <Typography sx={{fontWeight:"bold"}}>
+                  9.2
+                </Typography>
+              </Stack>
+            </Grid>
+            <Grid item xs={6}>
+             
+              <Stack sx={{padding:"0.5rem"}}>
+                <Typography variant="body1" sx={{color:"grey"}}>
+                  Roll Number
+                </Typography>
+                <Typography sx={{fontWeight:"bold"}}>
+                  AI2021001
+                </Typography>
+              </Stack>
+              <Stack sx={{padding:"0.5rem"}}>
+                <Typography variant="body1" sx={{color:"grey"}}>
+                  Specialization
+                </Typography>
+                <Typography sx={{fontWeight:"bold"}}>
+                  Machine Learning
+                </Typography>
+              </Stack>
+            </Grid>
+          </Grid>
+        </Box>
+
+       
+    
+            {/* Placement Information */}
+            <Box mb={2}>
+              <Typography fontWeight="bold" mb={1}>
+                Placement Information
+              </Typography>
+              <Grid container spacing={15}  sx={{padding:"1rem"}}>
+            <Grid item xs={6}>
+              <Box display="flex" alignItems="center" gap={1}>
+                <AttachMoneyIcon color="success" fontSize="medium" />
+                <Stack sx={{padding:"0.5rem"}}>
+                <Typography variant="body1" sx={{color:"grey"}}>Package</Typography>
+                <Typography variant="body1" color="green" sx={{fontWeight:700}}>
+                  $115K
+                </Typography>
+                </Stack>
+              </Box>
+              <Box display="flex" alignItems="center" gap={1}>
+                <CalendarTodayIcon  color="error" fontSize="medium" />
+                <Stack sx={{padding:"0.5rem"}}>
+                <Typography variant="body1" sx={{color:"grey"}}>Placement Year</Typography>
+                <Typography variant="body1" sx={{fontWeight:"bold"}}>2024</Typography>
+                </Stack>
+              </Box>
+            </Grid>
+            <Grid item xs={6}>
+              <Box display="flex" alignItems="center" gap={1}>
+                <LocationOnIcon fontSize="medium" />
+                <Stack sx={{padding:"0.5rem"}}>
+                 <Typography variant="body1" sx={{color:"grey"}}>Location</Typography>
+                <Typography variant="body1" sx={{fontWeight:"bold"}}>Redmond, WA</Typography>
+                </Stack>
+              </Box>
+              <Box display="flex" alignItems="center" gap={1}>
+                <WorkIcon color="info" fontSize="medium" />
+                <Stack sx={{padding:"0.5rem"}}>
+                 <Typography variant="body1" sx={{color:"grey"}}>Job Type</Typography>
+                <Typography variant="body1" sx={{fontWeight:"bold"}}>Full-time</Typography>
+                </Stack>
+              </Box>
+            </Grid>
+          </Grid>
+            </Box>
+    
+            {/* Key Skills */}
+            <Box mb={2}>
+              <Typography fontWeight="bold" mb={1}>
+                Key Skills
+              </Typography>
+              <Box display="flex" gap={1} flexWrap="wrap">
+                {["Python", "PyTorch", "Computer Vision", "OpenCV", "C++"].map(
+                  (skill) => (
+                    <Chip key={skill} label={skill} variant="outlined" sx={{ color: "#052517ff", backgroundColor: "#d3faf5ff" }} />
+                  )
+                )}
+              </Box>
+            </Box>
+    
+            {/* Quote / Remark */}
+            <Box
+              sx={{
+                backgroundColor: "#ffecec",
+                borderRadius: 2,
+                p: 2,
+                fontStyle: "italic",
+              }}
+            >
+              “Outstanding contribution to research projects and published papers
+              during undergraduate studies.”
+            </Box>
+          </DialogContent>
+          <DialogActions>
+              <Button
+                        component="a"
+                        href="https://www.linkedin.com/in/naveenkumar0320/"
+                        startIcon={<LinkedInIcon sx={{ px: "0.5rem" }} />}
+                        variant="contained"
+                        sx={{
+                            backgroundColor: "#3885f8ff",
+                            textTransform: "none",
+                            padding: "0.2rem 0.5rem",
+                            "& .MuiButton-startIcon > *": { fontSize: "35px !important" },
+                        }}
+                    >
+                        Linkedin
+                    </Button>
+          </DialogActions>
+        </Dialog>
         </>
     );
 }
